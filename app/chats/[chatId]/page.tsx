e00c5b1014caeb8e2ec9938b244d8ad3aad7046b
YoruLi <lionel3327@gmail.com>
2023-05-30 16:02:50 -0300

    add metadata for every chat

--- app/chats/[chatId]/page.tsx
+++ app/chats/[chatId]/page.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { supabase } from "@/utils/supabase";
 import Chat from "@/components/Chat";
 
+export async function generateMetadata({ params: { chatId } }: { params: { chatId: string } }) {
+    return { title: `${chatId} | chat` };
+}
 async function getProfileId() {
     const { data: profile } = await supabase.from("profiles").select();
 
@@ -10,7 +13,11 @@ async function getProfileId() {
     });
 }
 
-export default async function page({ params: { id } }: { params: { id: string } }) {
-    const { data } = await supabase.from("profiles").select().match({ id });
+interface PageProps {
+    params: {
+        chatId: string;
+    };
+}
+export default function page({ params: { chatId } }: PageProps) {
     return <Chat />;
 }
