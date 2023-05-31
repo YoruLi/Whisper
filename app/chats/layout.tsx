eef192aa3a5651bfc5ae84a3eb3f78acdad6080c
YoruLi <lionel3327@gmail.com>
2023-05-30 23:10:24 -0300

    fix height on mobile

--- app/chats/layout.tsx
+++ app/chats/layout.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import Chats from "@/components/Chats";
 import WithOutChats from "@/components/WithOutChats";
 import AppContext from "@/context/AppContext/AppContext";
@@ -7,7 +6,7 @@ import React, { useContext } from "react";
 export default function ChatsLayout({ children }: { children: React.ReactNode }) {
     const { state } = useContext(AppContext);
     return (
-        <div className="relative flex w-full">
+        <div className="relative flex w-full min-h-screen min">
             <Chats />
             {!(state.openedChat && state.openedChat.messages?.length !== 0) ? (
                 <WithOutChats />
